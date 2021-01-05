d6f4773b1793e1bd814c147563ca7a76bb7ca6f0
Abdallah Abd-elsabour <38044666+abdallah-abdelsabour@users.noreply.github.com>
2021-01-05 23:37:26 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,1 @@
-const structDatas = [
-    { handler: 'http', endpoint: 'http://localhost:3000/path', method: 'ALL' },
-    { handler: 'event', endpoint: 'http://localhost:3000/event', method: 'POST' },
-    { handler: 'GCS', endpoint: 'http://localhost:3000/GCS', method: 'POST' }
-];
-console.table(structDatas);
+
